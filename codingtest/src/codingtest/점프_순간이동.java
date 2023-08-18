172d4a0f8fd8034a284054b3dc4f4965f68b2888
hkw2304 <111471255+hkw2304@users.noreply.github.com>
2023-08-18 22:55:54 +0900

    점프_순간이동 성공

--- codingtest/src/codingtest/점프_순간이동.java
+++ codingtest/src/codingtest/점프_순간이동.java
@@ -4,58 +4,41 @@ import java.util.ArrayList;
 
 public class 점프_순간이동 {
 	public static void main(String[] args) {
-		int n = 6;
+		int n = 1;
 		int answer = dap(n);
-
 		System.out.println(answer);
 	}
 
 	private static int dap(int n) {
-		// TODO Auto-generated method stub
-		if (n == 1 || n == 2)
-			return 1;
-		else if (n == 3)
-			return 2;
 		ArrayList<Integer> arr = new ArrayList<>();
-		int temp = n;
-		while (temp > 3) {
-			if(temp / 2 == 2)
-				break;
-			arr.add(temp /= 2);
+		if(n == 1)
+			return 1;
+		for(int i = n; i > 1; i /= 2) {
+			arr.add(i / 2);
 		}
+		int i = 0;
+		int cnt = 0;
 		int len = arr.size() - 1;
-		int i = 2;
-		int cnt = 1;
-		while (true) {
-			if(i * 2 == n)
-				break;
-			else if((i + 1) * 2 == n) {
+		while(true) {
+			if(i + 1 == arr.get(len)) {
+				i++;
 				cnt++;
-				break;
 			}
-			else if((i * 2) + 1 == n) {
-				cnt++;
-				break;
+			if(i == n) {
+				return cnt;
 			}
-			if(i * 2 == arr.get(len)) {
+			else if(i * 2 == n) {
+				return cnt;
+			}
+			else if(i * 2 + 1 == n) {
+				return cnt + 1;
+			}
+			if(i == arr.get(len)) {
 				i *= 2;
 				len--;
 			}
-			else if(i + 1 == arr.get(len)) {
-				i++;
-				cnt++;
-				i++;
-				i *= 2;
-				cnt++;
-				len--;
-			}
-			else if(i * 2 + 1 ==arr.get(len)) {
-				cnt++;
-				len--;
-				i *= 2;
-				i++;
-			}
+			
 		}
-		return cnt;
+		
 	}
 }
