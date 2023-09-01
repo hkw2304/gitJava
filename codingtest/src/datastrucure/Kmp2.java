2b437b08aeca1022238df9cf03d929b4ab2d8c93
user <user@DESKTOP-LEA1EGQ>
2023-09-01 09:12:46 +0900

    update

---
+++ codingtest/src/datastrucure/Kmp2.java
@@ -0,0 +1,18 @@
+package datastrucure;
+
+public class Kmp2 {
+	public static void main(String[] args) {
+
+//	접두부 : 문자열의 왼쪽에서 시작
+//	접미부 : 문자열에서 오른쪽에서 시작
+//	경계 : 접두부와 접미부가 같은 경우 이 떄의 접두부와 접미부를 말한다.
+		
+//		1.처음 찾으려는 문자열의 접두부, 접미부를 찾는다.
+//		abacabaacabacabac
+//		abacabac
+//		접두부 : aba, 접미부 : aba, 경계 : c
+//		경계의 길이가 최대가 될 때까지 찾는다.
+//		2.접두부, 접미부의 경계를 찾고나서 인덱스를 접미부의 처음으로 옮긴다.
+		
+	}
+}
